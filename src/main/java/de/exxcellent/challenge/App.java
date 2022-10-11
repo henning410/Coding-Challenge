384c16cd1d62081bca40ce2a5ca12c559f414746
heweit00 <heweit00@hs-esslingen.de>
2022-10-11 17:30:37 +0200

    created function to get the field with the smallest spread for two specific columns

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -54,4 +54,33 @@ public final class App {
         }
         return records;
     }
+
+    /**
+     * Method to calculate the lowest spread between to given columns
+     *
+     * @param data Nested list which contains List of fields
+     * @param column1 Name of first column for spread calculation
+     * @param column2 Name of second column for spread calculation
+     * @return  Returns first field (called identifier) as String
+     */
+    private static String calcSmallestSpread(List<List<String>> data, String column1, String column2) {
+        // Make sure list has some content
+        if (!data.isEmpty()) {
+            List<String> headLine = data.get(0);
+            Double smallestSpread = null;
+            String identifier = "";
+            // Make sure to skip first line in iteration
+            for (List<String> line : data.subList(1, data.size())) {
+                double currentSpread = Math.abs(Double.parseDouble(line.get(headLine.indexOf(column1))) - Double.parseDouble(line.get(headLine.indexOf(column2))));
+                if (smallestSpread == null) {
+                    smallestSpread = currentSpread;
+                } else if (smallestSpread > currentSpread) {
+                    smallestSpread = currentSpread;
+                    identifier = line.get(0);
+                }
+            }
+            return identifier;
+        }
+        return "";
+    }
 }
