00fee6a595335f4b356229a94b7603db88a5088c
heweit00 <heweit00@hs-esslingen.de>
2022-10-11 17:26:04 +0200

    created function to read csv-file into list

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,13 @@
 package de.exxcellent.challenge;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -13,14 +21,6 @@ public final class App {
      * @param args The CLI arguments passed
      */
     public static void main(String... args) {
-        List<List<String>> records = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader("book.csv"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(COMMA_DELIMITER);
-                records.add(Arrays.asList(values));
-            }
-        }
         // Your preparation code …
 
         String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
@@ -28,5 +28,30 @@ public final class App {
 
         String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
+    }
+
+    /**
+     * Method to read csv file in resource folder and return lines as list
+     *
+     * @param filename
+     * @return List which contains list of fields
+     * @throws IOException  Throw exception if reading of the file fails
+     */
+    private static List<List<String>> readFileToList(String filename) throws IOException {
+        URL url = Thread.currentThread().getContextClassLoader().getResource(filename);
+        List<List<String>> records = new ArrayList<>();
+        if (url != null) {
+            InputStreamReader stream = new InputStreamReader(url.openStream());
+            try (BufferedReader br = new BufferedReader(stream)) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    String[] values = line.split(",");
+                    records.add(Arrays.asList(values));
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return records;
     }
 }
